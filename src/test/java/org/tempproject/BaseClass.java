a83d5c0b6d332569aa99584ef01018b0d2df32ab
vignesh7298 <vignesh7298@gmail.com>
2021-12-03 09:26:37 +0530

    bot method added

--- src/test/java/org/tempproject/BaseClass.java
+++ src/test/java/org/tempproject/BaseClass.java
@@ -93,10 +93,11 @@ public static WebDriver driver;
 		Select s = new Select(dropdown);
 		s.selectByValue("string");
 		
-		
-		
-
-	}
+		}
+	    private void vicky() {
+		System.out.println("vicky");
+		}
+	    
 	
 	
 	
